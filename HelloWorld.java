4c8a8014c927218255a320fb5f3cd356d3b38177
capgteam <capgteam22020@gmail.com>
2021-07-07 16:42:57 +0530

    Main method updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld{
 
 public static void main(String[] args){
 System.out.println("Hello! From Java!");
+System.out.println("+++++++++++++++++++++");
+System.out.println("Hello! From GitHub!");
+System.out.println("********************");
 }
 }
